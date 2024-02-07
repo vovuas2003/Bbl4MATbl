54f017356d7586a47065ee0a02283e5bd772754f
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2024-02-07 15:42:14 +0300

    Add files via upload

--- Task_1/2.py
+++ Task_1/2.py
@@ -8,22 +8,35 @@ def f(t, u, eps):
 def main():
     h = 10**-3
     l = -1.5
+    s = 0
     r = 0.55
     t = np.arange(l, r, h)
     n = len(t)
-    u = Solve(t, n, h, f, 0)
+    u = []
     plt.figure()
+    u = Solve(t, n, h, f, 0, s)
     plt.plot(t, u)
-    u = Solve(t, n, h, f, 10**-3)
-    plt.plot(t, u)
-    u = Solve(t, n, h, f, -10**-3)
-    plt.plot(t, u)
+    #
+    e = np.arange(-0.005, 0.005, 0.0001)
+    e = [0.001]
+    e=[]
+    #
+    for eps in e:
+        u = Solve(t, n, h, f, eps, s)
+        '''
+        if max(abs(u)) > 800:
+            print(eps)
+        '''
+        plt.plot(t, u)
+    plt.xlabel("t")
+    plt.ylabel("u(t)")
     plt.tight_layout()
     plt.grid()
     plt.show()
 
-def Solve(t, n, h, f, eps):
-    u = [0, 0]
+def Solve(t, n, h, f, eps, s):
+    u = [s]
+    u.append(u[0] + h * f(t[0], u[0], eps))
     for i in range(n - 2):
         u.append(Next(u[-2], u[-1], t[i], t[i + 1], h, f, eps))
     u = np.asarray(u)
